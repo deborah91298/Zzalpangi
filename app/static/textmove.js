d102e1bb20285008593e9aec7009065500df9b05
LionLin <deborah91298@hotmail.com>
2021-08-13 02:00:32 +0900

    words moving youngbumnim

--- app/static/textmove.js
+++ app/static/textmove.js
@@ -1,24 +1,39 @@
 let currentDroppable = null;
 
 result.onmousedown = function (event) {
-
   let shiftX = event.clientX - result.getBoundingClientRect().left;
   let shiftY = event.clientY - result.getBoundingClientRect().top;
-
   result.style.position = 'absolute';
   result.style.zIndex = 1000;
   document.body.append(result);
 
   moveAt(event.pageX, event.pageY);
+  
 
   function moveAt(pageX, pageY) {
-    console.log(pageX)
-    console.log(pageY)
     var position = document.getElementById("capture").getElementsByClassName("img-box")[0].getBoundingClientRect();
-    if (position.left < pageX && pageX < position.right && position.top < pageY && pageY < position.bottom) {
-      result.style.left = pageX - shiftX + 'px';
-      result.style.top = pageY - shiftY + 'px';
+    let shot = document.getElementById("shot").getBoundingClientRect();
+    if (shot.left < pageX && pageX < shot.right && shot.top < pageY && pageY < shot.bottom) {
+      return;
+    } else if (position.left < pageX && pageX < position.right && position.top < pageY && pageY < position.bottom) {
+      result.style.left = pageX +'px';
+      result.style.top = pageY + 'px';
+
+      let picX = result.style.left;
+      let picY = result.style.top;
+
+      function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+      setCookie("top", picY, 1);
+      setCookie("left", picX, 1);
+      console.log("move");
+      return;
     } else {
+      return;
     }
   }
 
